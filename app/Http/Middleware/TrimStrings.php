32a2161c6c2b008b549e72d4defd341e1efc3b04
Faustino Vasquez <faustino.vasquez@mitechnologiesinc.com>
2017-08-04 22:55:55 -0700

    almost upgrade

---
+++ app/Http/Middleware/TrimStrings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fvasquez
+ * Date: 4/08/17
+ * Time: 03:49 PM
+ */
+
+namespace App\Http\Middleware;
+
+
+class TrimStrings
+{
+
+}
---
+++ tests/TestsHelper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fvasquez
+ * Date: 4/08/17
+ * Time: 01:34 PM
+ */
+
+trait TestsHelper
+{
+
+}
